5258e56ac287e13c3a837f9feb10433d1221ba1c
gao <714496183@qq.com>
2018-02-08 20:46:23 +0800

    add

--- dmhy_crawler.js
+++ dmhy_crawler.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const moment = require('moment');
+const _ = require('lodash');
 const {
   timeout,
   isAheadDays,
@@ -8,22 +9,23 @@ const {
   warning
 } = require('./tools/tools.js');
 const R = require('ramda');
-const _ = require('lodash/fp');
 const fs = require('fs-extra');
 
-puppeteer.launch().then(async browser => {
+puppeteer.launch({ headless: false }).then(async browser => {
   const startTime = moment();
   let page = await browser.newPage();
-  await page.goto('https://share.dmhy.org/');
-  await page.waitForSelector('#topic_list');
+  await page.goto('https://share.dmhy.org/', { waitUntil: 'domcontentloaded' });
+  await page.waitForSelector('.jmd');
+  // await timeout(10000);
   info(`打开https://share.dmhy.org/ 耗时 ${moment().diff(moment(startTime), 'seconds', true)} 秒`);
-
+  page.on('console', msg => {
+    warning('msg' + msg);
+  });
   const comics = await page.evaluate(() => {
-    return document.querySelectorAll('#topic_list td');
+    return [...document.querySelectorAll('.jmd td a')].map(a => ({
+      href: a.href,
+      name: a.text
+    }));
   });
   console.log(comics);
-  // const trs = await page.$$('.weekly_list tr');
-  // const a = await page.$eval(".weekly_list tr", tr => tr);
-  // console.log('a.getProperties', a);
-  await browser.close();
 });
--- es6_crawler.js
+++ es6_crawler.js
@@ -22,7 +22,7 @@ puppeteer.launch().then(async browser => {
   //
   if (links) {
     const { ctime } = await fs.statSync('./data/es6-pdf/links.json');
-    if (isAheadDays(ctime, 7)) {
+    if (isAheadDays(ctime, 0)) {
       info('超过7天未拉取api列表，正在重新拉取...');
       links = undefined;
     } else {
@@ -36,57 +36,61 @@ puppeteer.launch().then(async browser => {
     await page.goto('http://es6.ruanyifeng.com/#README');
     await page.waitForSelector('#sidebar ol li a');
     links = await page.evaluate(() => {
+      // return [...document.querySelectorAll('#sidebar ol li a')].map(
+      //   (a, index) => ({
+      //     href: a.href.trim(),
+      //     name: `${index}.${a.text}`
+      //   })
+      // );
       return [...document.querySelectorAll('#sidebar ol li a')].map(
-        (a, index) => ({
-          href: a.href.trim(),
-          name: `${index}.${a.text}`
-        })
+        a => a
       );
-    });
+    }); 
+    console.log(links);
     info(`api列表抓取成功, 长度：${links.length}...`);
-    await fs.mkdirs('data/es6-pdf');
-    await fs.writeFile('./data/es6-pdf/links.json', JSON.stringify(links));
-    await page.close();
+    // await fs.mkdirs('data/es6-pdf');
+    // await fs.writeFile('./data/es6-pdf/links.json', JSON.stringify(links));
+    // await page.close();
   }
 
-  await fs.remove('./data/es6-pdf/pdf');
-  await fs.mkdirs('./data/es6-pdf/pdf');
+  // await fs.remove('./data/es6-pdf/pdf');
+  // await fs.mkdirs('./data/es6-pdf/pdf');
 
-  links = R.slice(0, Infinity)(links);
+  // links = R.slice(0, Infinity)(links);
 
-  const perChuck = 12;
-  const chuckedLinks = R.splitEvery(perChuck)(links);
+  // const perChuck = 12;
+  // const chuckedLinks = R.splitEvery(perChuck)(links);
 
-  info(`总共分成${chuckedLinks.length}个chuck, 每个chuck有${perChuck}个记录。共${links.length}个记录。`);
+  // info(`总共分成${chuckedLinks.length}个chuck, 每个chuck有${perChuck}个记录。共${links.length}个记录。`);
 
-  // 打开n个页面
-  await new Promise(async resolve => {
-    for (let i = 0; i < chuckedLinks.length; i++) {
-      const chuckedLink = chuckedLinks[i];
-      info(`chunk:${i}开始处理...`);
+  // // 打开n个页面
+  // await new Promise(async resolve => {
+  //   for (let i = 0; i < chuckedLinks.length; i++) {
+  //     const chuckedLink = chuckedLinks[i];
+  //     info(`chunk:${i}开始处理...`);
       
-      const pages = await Promise.all(
-        R.map(() => browser.newPage())(chuckedLink)
-      );
-      // 跳转n个链接
-      await Promise.all(
-        R.map(p => p[0].goto(p[1].href))(
-          R.zip(pages, chuckedLink)
-        )
-      );
+  //     const pages = await Promise.all(
+  //       R.map(() => browser.newPage())(chuckedLink)
+  //     );
+  //     // 跳转n个链接
+  //     await Promise.all(
+  //       R.map(p => p[0].goto(p[1].href))(
+  //         R.zip(pages, chuckedLink)
+  //       )
+  //     );
       
-      await Promise.all(
-        R.map(p =>
-          p[0].pdf({ path: `./data/es6-pdf/pdf/${p[1].name}.pdf` })
-        )(R.zip(pages, chuckedLink))
-      );
+  //     await Promise.all(
+  //       R.map(p =>
+  //         p[0].pdf({ path: `./data/es6-pdf/pdf/${p[1].name}.pdf` })
+  //       )(R.zip(pages, chuckedLink))
+  //     );
 
-      R.forEach(l => info(`${l.name}.pdf 保存成功`))(chuckedLink)
-      if (i === chuckedLinks.length - 1) {
-        resolve();
-      }
-    }
-  });
+  //     R.forEach(l => info(`${l.name}.pdf 保存成功`))(chuckedLink)
+  //     if (i === chuckedLinks.length - 1) {
+  //       resolve();
+  //     }
+  //   }
+  // });
   info(`处理结束，总耗时: ${moment().diff(moment(startTime), 'seconds', true)} 秒`);
   await browser.close();
 });
