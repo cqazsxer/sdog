0528e1490790b103e38b26c9f79e21534fa2bcd1
gaofeng <714496183@qq.com>
2018-02-08 13:19:34 +0800

    add

--- es6_crawler.js
+++ es6_crawler.js
@@ -1,39 +1,62 @@
 const puppeteer = require('puppeteer');
-const { timeout } = require('./tools/tools.js');
+const moment = require('moment');
+const { timeout, isAheadDays } = require('./tools/tools.js');
 const R = require('ramda');
-const fs = require('mz/fs');
+const fs = require('fs-extra');
 puppeteer.launch().then(async browser => {
-  let page = await browser.newPage();
+  // 检测上次抓取的api列表的时间 大于7天的话 就重新抓取
+  let links = await fs
+    .readFile('./data/es6-pdf/links.json')
+    .catch(() => console.error('api列表json不存在！, 正在抓取api列表...'));
+  if (links) {
+    const { ctime } = await fs.statSync('./data/es6-pdf/links.json');
+    if (isAheadDays(ctime, 0)) {
+      console.log('超过7天未拉取api列表，正在重新拉取...');
+      links = undefined;
+    }
+  }
 
-  await page.goto('http://es6.ruanyifeng.com/#README');
-  await page.waitForSelector('#sidebar ol li a');
-  let aTags = await page.evaluate(() => {
-    return [...document.querySelectorAll('#sidebar ol li a')].map(a => (
-      {
+  if (!links) {
+    let page = await browser.newPage();
+    await page.goto('http://es6.ruanyifeng.com/#README');
+    await page.waitForSelector('#sidebar ol li a');
+    links = await page.evaluate(() => {
+      return [...document.querySelectorAll('#sidebar ol li a')].map(a => ({
         href: a.href.trim(),
         name: a.text
-      }
-    ));
-  });
-  await fs.writeFile('./data/es6-pdf/links.json', JSON.stringify(aTags));
-  console.log(aTags.length);
-  await page.pdf({ path: `./data/es6-pdf/${aTags[0].name}.pdf` });
-  await page.close();
+      }));
+    });
+    console.log(`api列表抓取成功, 长度：${links.length}`);
+    await fs.mkdirs('data/es6-pdf');
+    // await page.pdf({path: `./data/es6-pdf/${links[0].name}.pdf`});
+    await fs.writeFile('./data/es6-pdf/links.json', JSON.stringify(links));
+    await page.close();
+  } else {
+    links = JSON.parse(links);
+    console.log(`api列表长度：${links.length}`);
+  }
+  // console.log(links.length);
+  // links = R.slice(23, Infinity)(links)
 
-  // // 这里也可以使用promise all，但cpu可能吃紧，谨慎操作
-  // for (var i = 1; i < aTags.length; i++) {
-  //   page = await browser.newPage()
+  // 打开n个页面
+  const pages = await Promise.all(R.map(() => browser.newPage())(links));
 
-  //   var a = aTags[i];
+  // 跳转n个链接
+  const gotoPages = await Promise.all(
+    R.map(p => p[0].goto(p[1].href, { timeout: 0 }))(R.zip(pages, links))
+  );
 
-  //   await page.goto(a.href);
-
-  //   await timeout(2000);
-
-  //   // await page.pdf({path: `./data/es6-pdf/${a.name}.pdf`});
-
-  //   page.close();
-  // }
+  // 在每个页面搞一些事情
+  await fs.remove('./data/es6-pdf/pdf');
+  await timeout(1000);
+  await fs.mkdirs('./data/es6-pdf/pdf');
+  console.log('开始打印pdf...');
+  await Promise.all(
+    R.map(p => p[0].pdf({ path: `./data/es6-pdf/pdf/${p[2]}.${p[1].name}.pdf` }))(
+      R.zip(pages, links, R.range(0, R.length(links)))
+    )
+  );
+  console.log('pdf 下载成功');
 
   await browser.close();
 });
--- tools/tools.js
+++ tools/tools.js
@@ -1,16 +1,20 @@
+const moment = require('moment');
+
 class Tools {
   static timeout(delay) {
     return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            try {
-                resolve(1)
-            } catch (e) {
-                reject(0)
-            }
-        }, delay)
-    })
+      setTimeout(() => {
+        try {
+          resolve(1);
+        } catch (e) {
+          reject(0);
+        }
+      }, delay);
+    });
   }
-
+  static isAheadDays(m, long) {
+    return moment().diff(moment(m), 'days', true) > long;
+  }
   /**
    * [TimeTools description]
    * @param {[type]} timestamp  12312312312312
@@ -21,21 +25,27 @@ class Tools {
    * D: date 0 ~ 31
    */
   static moment(formatStr, timestamp) {
-    let date = new Date(timestamp || new Date().getTime())
+    let date = new Date(timestamp || new Date().getTime());
 
-    let M = date.getMonth() + 1
+    let M = date.getMonth() + 1;
 
-    let Y = date.getFullYear()
+    let Y = date.getFullYear();
 
-    let D = date.getDate()
+    let D = date.getDate();
 
-    let h = date.getHours()
+    let h = date.getHours();
 
-    let m = date.getMinutes()
+    let m = date.getMinutes();
 
-    let s = date.getSeconds()
+    let s = date.getSeconds();
 
-    return formatStr.replace('M', M).replace('Y', Y).replace('D', D).replace('h', h).replace('m', m).replace('s', s)
+    return formatStr
+      .replace('M', M)
+      .replace('Y', Y)
+      .replace('D', D)
+      .replace('h', h)
+      .replace('m', m)
+      .replace('s', s);
   }
 }
 
